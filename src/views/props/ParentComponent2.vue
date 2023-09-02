61cdaad8a378a12b66dab47fd5942be807b68a65
unknown <admin@admin.com>
2023-09-02 09:26:05 +0900

    업데이트

--- src/views/props/ParentComponent2.vue
+++ src/views/props/ParentComponent2.vue
@@ -2,7 +2,7 @@
   <child-component ref="child_component" />
 </template>
 <script>
-import ChildComponent from '@/views/props//ChildComponent2'
+import ChildComponent from '@/views/props/ChildComponent2'
 export default {
   //부모컴포넌트에서 자식 컴포넌트의 함수 직접 호출하기
 
